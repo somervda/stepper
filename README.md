cb7b71e89ccabf4822546fd777467bb2eb128aaf
David <39265971+somervda@users.noreply.github.com>
2022-01-07 12:16:58 -0500

    Added rotary encoder interface. Initially set up to range between -100 and 100 as values.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Used to experiment with using a RPI Pico and a stepper motor via a TB6600
 controller
 
-Doen the road I want to use a stepper motor to run the lead screw on a 7x14 chinese
+Down the road I want to use a stepper motor to run the lead screw on a 7x14 chinese
 mini-lathe. 
--- tester.py
+++ tester.py
@@ -1,1 +1,26 @@
-# Main routine for experimenting and testing my stepper moter interface
+# Main routine for experimenting and testing my stepper motor interface
+
+import time
+import sys
+from rotary_irq import RotaryIRQ
+
+
+r = RotaryIRQ(pin_num_clk=15,
+              pin_num_dt=14,
+              min_val=-100,
+              max_val=100,
+              reverse=True,
+              range_mode=RotaryIRQ.RANGE_BOUNDED)
+
+r.set(value=0)
+val_old = r.value()
+print("Starting...")
+while True:
+    val_new = r.value()
+
+    if val_old != val_new:
+        val_old = val_new
+        print('result =', val_new)
+
+    time.sleep_ms(50)
+print("End")
